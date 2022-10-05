fab69b16c02c2b387ddb2c951acf0e21f0dc87f7
Matthew Todor <unclejunglegeorge@gmail.com>
2022-10-05 16:15:08 -0600

    added proper favicon as well as replaced banner text with actual logo. Refactored head-banner to output a different setup based on screen-width

--- src/components/head-banner.js
+++ src/components/head-banner.js
@@ -1,43 +1,50 @@
 import React from "react";
-import { Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import "./component.css";
+import { useMediaQuery, useTheme } from "@mui/material";
 import Image from "../assets/pictures/WoWScrnShot_051522_210632.jpg";
+import RecycledLogoText from "../assets/pictures/recycled_logo_fixt_it_ONLY_text.png";
 const Banner = () => {
+	const theme = useTheme();
+
+	const mobile = useMediaQuery(theme.breakpoints.up("sm"));
+
 	return (
-		<Grid
-			container
-			maxWidth="xl"
-			sx={{
-				m: "auto",
-			}}
-		>
-			<Grid
-				item
-				xs={12}
-				sx={{
-					backgroundImage: `url(${Image})`,
-					backgroundPosition: "center",
-					objectFit: "cover",
-					height: 400,
-					display: {
-						xs: "none",
-						sm: "block",
-					},
-				}}
-			></Grid>
-			<Grid
-				container
-				xs={12}
-				sx={{
-					justifyContent: "center",
-				}}
-			>
-				<Typography variant="h2" color="primary.main" component="h1">
-					recycled
-				</Typography>
-			</Grid>
-		</Grid>
+		<>
+			{mobile ? (
+				<Grid
+					container
+					maxWidth="xl"
+					sx={{
+						m: "auto",
+					}}
+				>
+					<Grid
+						container
+						xs={12}
+						sx={{
+							backgroundImage: `url(${Image})`,
+							backgroundPosition: "45% 42%",
+							height: 400,
+							display: {
+								xs: "none",
+								sm: "block",
+							},
+						}}
+					>
+						<Grid item xs={12}>
+							<img src={RecycledLogoText} alt="recycled logo" className="banner-logo-main" />
+						</Grid>
+					</Grid>
+				</Grid>
+			) : (
+				<Grid container>
+					<Grid item sx={12}>
+						<img src={RecycledLogoText} alt="recycled logo" className="banner-logo-mobile" />
+					</Grid>
+				</Grid>
+			)}
+		</>
 	);
 };
 
